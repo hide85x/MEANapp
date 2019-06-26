f562b2cd32955844388abddf38ada815ed117227
hide <hide85x@gmail.com>
2019-06-26 23:37:42 +0200

    initial

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+
+    posts= []
+    // posts = [
+    //     {
+    //         title: 'shit1',
+    //         content: " double shit ssasd"
+    //     },
+    //     {
+    //         title: 'shit2',
+    //         content: " double shit ssasd"
+    //     },
+    //     {
+    //         title: 'shit3',
+    //         content: " double shit ssasd"
+    //     }
+    // ]
+}
